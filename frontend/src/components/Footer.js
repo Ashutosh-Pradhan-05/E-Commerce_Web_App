d629b4efe04309ad0f02b17aacf19edc868f0ada
Ashutosh Pradhan <iamashutoshpradhan@gmail.com>
2025-03-20 10:31:10 +0530

    add files

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='bg-slate-200 mt-[63px]'>
+      <div className='container mx-auto p-4'>
+       <p className='text-center font-bold' title="Youtube Channel">Dynamic Coding with Ashu</p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer;
+
+//all clear but added mt-63px
