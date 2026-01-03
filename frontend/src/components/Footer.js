d8b75f8b49c13c5e273bd3f776d47560fea9c6b5
Ashutosh Pradhan <iamashutoshpradhan@gmail.com>
2026-01-03 20:00:03 +0530

    node_modules and some necessary files hidden through .gitignore.

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className='bg-slate-200 mt-[63px]'>
       <div className='container mx-auto p-4'>
-       <p className='text-center font-bold' title="Youtube Channel">Dynamic Coding with Ashu</p>
+       <p className='text-center font-bold' title="Youtube Channel">Designed and Developed by Ashutosh 💝</p>
       </div>
     </footer>
   )
